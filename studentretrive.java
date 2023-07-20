63bc455a8e9a7385c9e8d4af712ee376b072bf7e
Rajwinderkaur456 <116958294+Rajwinderkaur456@users.noreply.github.com>
2023-07-20 09:54:29 -0400

    Create studentretrive.java

---
+++ studentretrive.java
@@ -0,0 +1,23 @@
+package StudentPck;
+
+import java.util.LinkedList;
+
+public class Studentretrive {
+    String name;
+    float grade;
+
+    public Studentretrive(String name, float grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getGrade() {
+        return grade;
+    }
+}
+
+
